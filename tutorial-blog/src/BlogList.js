def8f04ffec002795cd65bb518ee1e3aecf57b83
Siongyu <2837325L@student.gla.ac.uk>
2023-12-11 17:43:09 +0800

    Completed Basic React tutorial
    With a basic blog project

---
+++ tutorial-blog/src/BlogList.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const BlogList = ({ blogs, title }) => {
+
+    return (
+        <div className="blog-list">
+            <h2>{ title }</h2>
+            {blogs.map((blogItem) => (
+                <div className="blog-preview" key={blogItem.id}>
+                    <Link to={`/blogs/${blogItem.id}`}>
+                        <h2>{ blogItem.title }</h2>
+                        <p>Written by { blogItem.author }</p>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default BlogList;
